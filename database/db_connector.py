d731bf393d0c9d9d6c1db1ddda51c2ba74ada129
spencerdv <52266770+spencerdv@users.noreply.github.com>
2024-01-21 19:55:31 -0500

    Update db_connector.py

--- database/db_connector.py
+++ database/db_connector.py
@@ -1,19 +1,68 @@
-# To actually have your app use this file, you need to RENAME the file to db_credentials.py
-# You will find details about your CS340 database credentials on Canvas.
+import MySQLdb
+import os
+from dotenv import load_dotenv, find_dotenv
 
-# the following will be used by the webapp.py to interact with the database
-# You can also use environment variables
+# Load our environment variables from the .env file in the root of our project.
+load_dotenv(find_dotenv())
 
-# For Local Devlelopment
-host = 'localhost'
-user = 'root'                                   # can be different if you set up another username in your MySQL installation
-passwd = ''                        # set accordingly
-db = 'mysql'
+# Set the variables in our application with those environment variables
+host = "classmysql.engr.oregonstate.edu"
+user = "cs340_mahmoudk"
+passwd = "7813"
+db = "cs340_mahmoudk"
+cursorClass = "DictCursor"
+
+def connect_to_database(host = host, user = user, passwd = passwd, db = db):
+    '''
+    connects to a database and returns a database objects
+    '''
+    db_connection = MySQLdb.connect(host,user,passwd,db)
+    return db_connection
+
+def execute_query(db_connection = None, query = None, query_params = ()):
+    '''
+    executes a given SQL query on the given db connection and returns a Cursor object
+
+    db_connection: a MySQLdb connection object created by connect_to_database()
+    query: string containing SQL query
+
+    returns: A Cursor object as specified at https://www.python.org/dev/peps/pep-0249/#cursor-objects.
+    You need to run .fetchall() or .fetchone() on that object to actually acccess the results.
+
+    '''
+
+    if db_connection is None:
+        print("No connection to the database found! Have you called connect_to_database() first?")
+        return None
 
 
-# For OSU Flip Servers
+    if query is None or len(query.strip()) == 0:
+        print("query is empty! Please pass a SQL query in query")
+        return None
 
-# host = 'classmysql.engr.oregonstate.edu'      # MUST BE THIS
-# user = '<your-cs-340-db-username-here>'       # don't forget the cs340_ prefix
-# passwd = '<your-password-here>'               # should only be 4 digits if default
-# db = '<name-of-database-on-osu-server>'  
+    print("Executing %s with %s" % (query, query_params));
+    # Create a cursor to execute query. Why? Because apparently they optimize execution by retaining a reference according to PEP0249
+    cursor = db_connection.cursor(MySQLdb.cursors.DictCursor)
+    
+    '''
+    params = tuple()
+    #create a tuple of paramters to send with the query
+    for q in query_params:
+        params = params + (q)
+    '''
+    #TODO: Sanitize the query before executing it!!!
+    cursor.execute(query, query_params)
+    # this will actually commit any changes to the database. without this no
+    # changes will be committed!
+    db_connection.commit();
+    return cursor
+
+if __name__ == '__main__':
+    print("Executing a sample query on the database using the credentials from db_credentials.py")
+    db = connect_to_database()
+    query = "SELECT * from students;"
+    results = execute_query(db, query);
+    print("Printing results of %s" % query)
+
+    for r in results.fetchall():
+        print(r)
