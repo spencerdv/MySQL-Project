4ff4fe15d3c9851f9a8a96cecb6a2d7324de0f2e
spencerdv <52266770+spencerdv@users.noreply.github.com>
2024-01-21 19:55:49 -0500

    Update db_credentials.py

--- database/db_credentials.py
+++ database/db_credentials.py
@@ -1,12 +1,19 @@
-# Handles communcation w the Database
-
 # To actually have your app use this file, you need to RENAME the file to db_credentials.py
 # You will find details about your CS340 database credentials on Canvas.
 
 # the following will be used by the webapp.py to interact with the database
 # You can also use environment variables
 
-host = 'classmysql.engr.oregonstate.edu'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
-user = 'cs340_verhages'
-passwd = '9139'
-db = 'cs340_verhages'
+# For Local Devlelopment
+host = 'localhost'
+user = 'root'                                   # can be different if you set up another username in your MySQL installation
+passwd = ''                        # set accordingly
+db = 'mysql'
+
+
+# For OSU Flip Servers
+
+# host = 'classmysql.engr.oregonstate.edu'      # MUST BE THIS
+# user = '<your-cs-340-db-username-here>'       # don't forget the cs340_ prefix
+# passwd = '<your-password-here>'               # should only be 4 digits if default
+# db = '<name-of-database-on-osu-server>'  
